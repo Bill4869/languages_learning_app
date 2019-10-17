d0cc1951e57efab46a69ef1afb917cc89778cc46
Bill4869 <31407241+Bill4869@users.noreply.github.com>
2019-10-17 22:42:47 +0900

    Update functions.php

--- service/model/functions.php
+++ service/model/functions.php
@@ -4,12 +4,14 @@ define('DB_DATABASE', 'teamEdb');
 define('DB_USERNAME', 'teamEdbuser');
 define('DB_PASSWORD', 'teamEdbuser');
 define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
-
+define('DB_PERSISTENCE', 1);
 function getPDO()
 {
     static $pdo;
     if (!isset($pdo)) {
-        $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+        $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD, 
+        array(PDO::ATTR_PERSISTENT=>DB_PERSISTENCE, 
+        PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
         $pdo->setattribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
         $pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
