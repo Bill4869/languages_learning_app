de6fa85163f22b9eac054fe3aff9b7dbf3831df0
Bill4869 <31407241+Bill4869@users.noreply.github.com>
2019-10-23 14:11:57 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install mysql-server -y
 sudo /usr/bin/mysql_secure_installation
 
 sudo apt-get install php -y
-sudo apt-get install -y php-{bcmath,bz2,intl,gd,mbstring,mcrypt,mysql,zip} && sudo apt-get install
+sudo apt-get install -y php-{bcmath,bz2,intl,gd,mbstring,mcrypt,mysql,zip} && sudo apt-get install libapache2-mod-php -y
 sudo apt-get install php-mysql
 
 systemctl restart apache2.service
